4ed400a56147266f313743488a9d700ac57c8c08
Renata Oliveira <renatabels@gmail.com>
2018-04-24 00:32:53 -0300

    :lipstick: Adding Customer Initial UI

--- src/components/sales/Customer.js
+++ src/components/sales/Customer.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Customer = props => {
-    return (
-        <div className="row">
-            <div className="col">
-                <form>
+class Customer extends Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <div className="row">
+                <div className="col">
                     <fieldset>
-                        
+                        <form className="form-group row my-2 my-lg-0 d-flex justify-content-center ">
+                            <div className="col-lg-12 d-flex justify-content-center mb-5">
+                                <input className="form-control col-md-4 mt-5 align-self-center text-center" type="search" placeholder="Search" aria-label="Search" />
+                            </div>
+                            <hr style={{ color: '#757575', width: '90%' }} className="d-flex justify-content-center " />
+                            <br />
+                            <button className="btn btn-primary col-md-2 mt-5 ml-2 align-self-center" type="submit">Novo Cliente</button>
+                        </form>
+                        <button type="submit" className="btn btn-primary mt-5 float-right">Salvar</button>
+                        <button type="submit" className="btn btn-primary mt-5 mr-2 float-right">Voltar</button>
                     </fieldset>
-                    <button type="submit" className="btn btn-primary">Salvar</button>
-                </form>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Customer
--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import CssBaseline from 'material-ui/CssBaseline'
 
 const app = (
     <React.Fragment>
-        <CssBaseline />
         <App />
     </React.Fragment>
 )
