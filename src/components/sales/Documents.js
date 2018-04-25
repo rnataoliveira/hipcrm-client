4369b279a9138d2684a464277c00617bcf9c7b6f
Renata Oliveira <renatabels@gmail.com>
2018-04-25 16:45:42 -0300

    :lipstick: Adding Documents ui

--- src/components/sales/Documents.js
+++ src/components/sales/Documents.js
@@ -1,5 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { withRouter } from "react-router-dom";
 
-const Documents = () => (<h1>Documents</h1>)
+class Documents extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    handleGoBack(event) {
+        this.props.history.push("/sales/new/appointments");
+    }
+
+    handleGogoForward(event) {
+        this.props.history.push("/sales/new/agreement");
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Documents</h1>
+                <form>
+                    <div class="input-group mb-3">
+                        <div class="custom-file">
+                            <input type="file" class="custom-file-input" id="inputGroupFile02" />
+                            <label class="custom-file-label" for="inputGroupFile02">Carregar Arquivos</label>
+                        </div>
+                        <div class="input-group-append">
+                            <span class="input-group-text" id="">+</span>
+                        </div>
+                    </div>
+                    <button onClick={this.handleGogoForward.bind(this)} type="button" className="btn btn-primary mt-5 mr-2 float-right">Next</button>
+                    <button type="button" className="btn btn-primary mt-5 mr-2 float-right">Upload</button>
+                    <button onClick={this.handleGoBack.bind(this)}  type="button" className="btn btn-primary mt-5 mr-2 float-right">Anterior</button>
+                </form>
+            </div>
+        )
+    }
+}
 
 export default Documents
