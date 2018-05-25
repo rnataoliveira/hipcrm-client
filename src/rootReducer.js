a8566be1e045f7ff03031838f718e70cbc095ac9
Renata Oliveira <renatabels@gmail.com>
2018-05-25 14:32:00 -0300

    :sparkles: Passing contracts reducer

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -4,10 +4,12 @@ import { routerReducer } from 'react-router-redux'
 import { reducer as oidcReducer } from 'redux-oidc'
 import { reducer as salesReducer } from './sales-pipeline'
 import { reducer as customersReducer } from './customers'
+import { reducer as contractsReducer } from './contracts'
 
 export default combineReducers({
   routing: routerReducer,
   oidc: oidcReducer,
   salesPipeline: salesReducer,
-  customers: customersReducer
+  customers: customersReducer,
+  contracts: contractsReducer
 })
