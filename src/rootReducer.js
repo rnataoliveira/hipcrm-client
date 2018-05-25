47501c82034a931ea7957cbdc55206516711d974
Renata Oliveira <renatabels@gmail.com>
2018-05-25 15:57:52 -0300

    :sparkles: Adding reducer from report to rootReducer

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -5,11 +5,13 @@ import { reducer as oidcReducer } from 'redux-oidc'
 import { reducer as salesReducer } from './sales-pipeline'
 import { reducer as customersReducer } from './customers'
 import { reducer as contractsReducer } from './contracts'
+import { reducer as reportsReducer } from './reports'
 
 export default combineReducers({
   routing: routerReducer,
   oidc: oidcReducer,
   salesPipeline: salesReducer,
   customers: customersReducer,
-  contracts: contractsReducer
+  contracts: contractsReducer,
+  reports: reportsReducer
 })
