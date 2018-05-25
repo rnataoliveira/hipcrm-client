e66c2a411c439a82273954d86a021bb0901ad333
Renata Oliveira <renatabels@gmail.com>
2018-05-25 08:40:49 -0300

    :rocket: Changing auth callback url

--- src/auth/userManager.js
+++ src/auth/userManager.js
@@ -5,7 +5,7 @@ const config = {
   client_id: '42472227382-lv313luvu3etp0ck6vnfv67jj06kilv0.apps.googleusercontent.com',
   scope: `profile email openid https://www.googleapis.com/auth/calendar`,
   response_type: 'token id_token',
-  redirect_uri: 'http://localhost:3000/auth/callback'
+  redirect_uri: 'https://corretora-lopes.firebaseapp.com/auth/callback'
 }
 
 export default createUserManager(config)
