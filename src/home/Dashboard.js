5370ed402b2f9f50f7d7e0925f6a23ceab95318d
Renata Oliveira <renatabels@gmail.com>
2018-06-07 02:18:06 -0300

    :bug: Reponsive shit

--- src/home/Dashboard.js
+++ src/home/Dashboard.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Dashboard = props => {
   return (
-    <div>
-      <div className="row mt-4">
+    <div className="row mb-5">
+      <div className="mt-4">
         <h1>Compromissos do dia</h1>
       </div>
       <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=3bml9bcuspor2r1qt2jf0j0gv0%40group.calendar.google.com&amp;color=%23B1365F&amp;src=bo527cmein5cgautvg3ipqfb94%40group.calendar.google.com&amp;color=%23AB8B00&amp;ctz=America%2FSao_Paulo"
