b0acb180c889166f629d367407838988451f64cb
Renata Oliveira <renatabels@gmail.com>
2018-06-08 19:36:36 -0300

    pub cfg

--- src/sales-pipeline/components/Documents.js
+++ src/sales-pipeline/components/Documents.js
@@ -19,6 +19,7 @@ class Documents extends Component {
       const picker = new window.google.picker.PickerBuilder()
         .addView(uploadView)
         .setOAuthToken(this.props.oAuthToken)
+        .setOrigin(window.location.protocol + '//' + window.location.host)
         // .setDeveloperKey('AIzaSyBjauEuKZPDHAO9G1C8QolA3ljnpUvXaXM')
         .build()
 
