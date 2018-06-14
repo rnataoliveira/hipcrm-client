6f8ad0e875d76f6dea733907724507561c6efd08
Renata Oliveira <renatabels@gmail.com>
2018-06-14 03:36:05 -0300

    :sparkles: Agreement list

--- src/agreements/components/AgreementList.js
+++ src/agreements/components/AgreementList.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
-import { fetchAgreements, filterAgreements } from '../actions'
-import { connect } from 'react-redux'
-import { flashMessage } from '../../flash-messages/actions'
+import { fetchAgreements } from '../actions'
 
 class AgreementList extends Component {
   componentDidMount() {
     this.props.fetchAgreements()
   }
-  
-  // componentWillUnmount() {
-  //   this.props.agreementId && this.props.displaySuccess()
-  // }
 
   handleChange(event) {
     this.props.filterAgreements(event.target.value)
   }
 
   render() {
-    console.log(this.props)
+    console.log(this.props, 'props')
     return (
       <div>
         <h1 className="mt-4">Contratos</h1>
@@ -31,22 +25,24 @@ class AgreementList extends Component {
         <table className="table mt-2">
           <thead>
             <tr>
-              <th scope="col">Venda</th>
+              <th scope="col">Número</th>
+              <th scope="col">Código da Venda</th>
               <th scope="col">Cliente</th>
               <th scope="col"></th>
             </tr>
           </thead>
           <tbody>
-            {this.props.agreements.map(a => {
-              <tr key={a.number}>
-                <th scope="row">0009</th>
-                <td>Renata Oliveira</td>
+            {this.props.agreements.map(a =>
+              <tr key={a.id}>
+                <th scope="row">{a.number}</th>
+                <td>{a.sale.code}</td>
+                <td>{a.sale.customer.personalData.companyName}</td>
                 <td className="text-right">
                   <Link className="m-2" to="">Detalhes</Link>
                   <button type="button" className="btn btn-primary m-2">Dar Baixa</button>
                 </td>
               </tr>
-            })}
+            )}
           </tbody>
         </table>
       </div>
@@ -61,15 +57,4 @@ AgreementList.propTypes = {
   filterAgreements: PropTypes.func.isRequired
 }
 
-
-const mapStateToProps = state => ({
-  agreements: state.agreements.all,
-})
-
-const mapDispatchToProps = dispatch => ({
-  fetchAgreements: () => dispatch(fetchAgreements()),
-  filterAgreements: q => dispatch(filterAgreements(q)),
-  displaySuccess: () => dispatch(flashMessage({ text: 'Dar Baixa executado com sucesso!' }))
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(AgreementList)
+export default AgreementList
--- src/agreements/containers/AgreementListContainer.js
+++ src/agreements/containers/AgreementListContainer.js
@@ -1,22 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchAgreements, filterAgreements } from '../actions'
-
+import { flashMessage } from '../../flash-messages/actions'
 import AgreementList from '../components/AgreementList'
+import PropTypes from 'prop-types'
 
-const applyFilter = (agreement, q) => {
-  return q ? this.props.agreements.filter(agreement => agreement.number !== q) : agreement
+
+const applyFilter = (agreements, q) => {
+  return q ? this.props.agreements.filter(agreement => agreement.number === q) : agreements
 }
 
 const mapStateToProps = state => ({
-  // customers: applyFilter(state.customers.all, state.customers.filter),
   agreements: applyFilter(state.agreements.all, state.agreements.filter),
   filter: state.agreements.filter
 })
 
 const mapDispatchToProps = dispatch => ({
-  feetchAgreements: () => dispatch(fetchAgreements()),
-  filterAgreements: q => dispatch(filterAgreements(q))
+  fetchAgreements: () => dispatch(fetchAgreements()),
+  filterAgreements: q => dispatch(filterAgreements(q)),
+  displaySuccess: () => dispatch(flashMessage({ text: 'Dar Baixa executado com sucesso!' }))
 })
+
+AgreementList.propTypes = {
+  agreements: PropTypes.array,
+  filter: PropTypes.string,
+  fetchAgreements: PropTypes.func.isRequired,
+  filterAgreements: PropTypes.func.isRequired
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(AgreementList)
